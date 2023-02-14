be3b457a1f4e7d7fac2200a747dfba1fd4942ad5
mo <nataleshen.ko@yandex.ru>
2023-02-14 14:13:59 +0300

    commit-check-tests

--- src/main/java/org/malinaink/UserRepository.java
+++ src/main/java/org/malinaink/UserRepository.java
@@ -1,4 +1,33 @@
 package org.malinaink;
 
+import java.util.*;
+
 public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    public Collection<User> getAllUsers() {
+        return Collections.unmodifiableCollection(users);
+    }
+
+    public Optional<User> findByLogin(String login) {
+        return this.getAllUsers()
+                .stream()
+                .filter(u -> u.getLogin().equalsIgnoreCase(login))
+                .findFirst();
+    }
+
+    public Optional<User> findByLoginAndPassword(String login, String password) {
+        return this.getAllUsers().stream()
+                .filter(u -> u.getLogin() != null && u.getLogin().equalsIgnoreCase(login))
+                .filter(u -> u.getPassword() != null && u.getPassword().equals(password))
+                .findFirst();
+    }
+
+    public User addUser(User user) {
+        this.users.add(user);
+        return user;
+    }
 }
+
+
--- src/main/java/org/malinaink/UserService.java
+++ src/main/java/org/malinaink/UserService.java
@@ -1,4 +1,45 @@
 package org.malinaink;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean createUser(String login, String password) {
+        if (login == null || login.isBlank() || password == null || password.isBlank()) {
+            throw new IllegalArgumentException("User should be defined");
+        }
+        boolean userExist = this.userRepository
+                .getAllUsers()
+                .stream()
+                .anyMatch(t -> t.getLogin().equals(login));
+        if (userExist) {
+            throw new UserNonUniqueException("User already exist");
+        }
+        User user = new User(login, password);
+        this.userRepository.addUser(user);
+        return false;
+    }
+
+
+    public List<String> getLoginsOfAllUsers() {
+        try {
+            return this.userRepository
+                    .getAllUsers()
+                    .stream()
+                    .map(User::getLogin)
+                    .collect(Collectors.toList());
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
